44fe6726a778d46e84c7e40b1241dc8d19170db5
lianghaohui <1285226024@qq.com>
2023-09-06 13:08:12 +0800

    fix

--- second-demo-project/src/main/java/com/ggs/fisrt/SecondDemoProjectApplication.java
+++ second-demo-project/src/main/java/com/ggs/fisrt/SecondDemoProjectApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SecondDemoProjectApplication {
 
+    //111
     public static void main(String[] args) {
         SpringApplication.run(SecondDemoProjectApplication.class, args);
     }
